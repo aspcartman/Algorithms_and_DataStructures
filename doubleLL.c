f25d77b74133fd9052611394312afcd2cae0d570
kvhq <kvhqll@gmail.com>
2017-04-19 01:53:01 +0300

    Update doubleLL.c

--- doubleLL.c
+++ doubleLL.c
@@ -2,14 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void dll_init(DoubleLinkedList *list)
-{
-    list = malloc(sizeof(list));
+struct Node {
+    int data;
+    Node *previous;
+    Node *next;
+};
+
+struct DoubleLinkedList {
+    int size;
+    Node *head;
+    Node *tail;
+};
+
+void dllInit(DoubleLinkedList *list) {
+    list = malloc(sizeof(DoubleLinkedList));
+    list->size = 0;
+    list->head = NULL;
+    list->tail = NULL;
 }
 
-void dll_place_before(DoubleLinkedList *list, int element)
-{
-    Node *temp = malloc(sizeof(*temp));
+void dllPlaceBefore(DoubleLinkedList *list, int element) {
+    Node *temp = malloc(sizeof(Node));
     temp->data = element;
     temp->next = list->head;
     if (list->size == 0)
@@ -17,13 +30,12 @@ void dll_place_before(DoubleLinkedList *list, int element)
     else
         list->head->previous = temp;
 
-    list->heap = temp;
+    list->head = temp;
     list->size++;
 }
 
-void dll_place_after(DoubleLinkedList *list, int element)
-{
-    Node *temp = malloc(sizeof(*temp));
+void dllPlaceAfter(DoubleLinkedList *list, int element) {
+    Node *temp = malloc(sizeof(Node));
     temp->data = element;
     temp->previous = list->tail;
     if (list->size == 0)
