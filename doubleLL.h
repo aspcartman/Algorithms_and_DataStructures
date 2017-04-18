66354b79c1df36a2d9c12dadbd937307d4bfc12e
kvhq <kvhqll@gmail.com>
2017-04-19 01:48:56 +0300

    Update doubleLL.h

--- doubleLL.h
+++ doubleLL.h
@@ -1,21 +1,9 @@
-typedef struct SimpleNode {
-	int data;
-	struct SimpleNode *previous;
-	struct SimpleNode *next;
-} Node;
+typedef struct Node Node;
 
-typedef struct {
-	int size;
-	Node *head;
-	Node *tail;
-} DoubleLinkedList;
+typedef struct DoubleLinkedList DoubleLinkedList;
 
-void dll_init(DoubleLinkedList *list);
+void dllInit(DoubleLinkedList *list);
 
-void dll_place_before(DoubleLinkedList *list, int element);
+void dllPlaceBefore(DoubleLinkedList *list, int element);
 
-void dll_place_after(DoubleLinkedList *list, int element);
-
-int dll_get_element(DoubleLinkedList *list, int index);
-
-int dll_remove_element(DoubleLinkedList *list, int index);
+void dllPlaceAfter(DoubleLinkedList *list, int element);
